733b985faed5658c00067340d29e2f80977802e1
Emma Foucault <foucault.emma@gmail.com>
2019-12-19 19:12:17 +0100

    decomment bootstrap

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,25 @@
 class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    raise
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    Restaurant.new(params[:restaurant])
+    Restaurant.save
+  end
+
+  private
+
+  #def restaurant_params
+    #params.
+  #end
 end
